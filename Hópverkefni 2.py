bf1846ede7089a6b36dc97df2d57f41c83d929af
palligunn <palligunn21@gmail.com>
2016-11-25 08:33:49 +0000

    Add files via upload

---
+++ Hópverkefni 2.py
@@ -0,0 +1,50 @@
+#hópverkefni 2
+#jóhann Chanse Sigurðsson og Páll Gunnar Svansson
+svar = valmynd
+while svar = valmynd:
+    print ("1.Bílar")
+    print ("2.Samlagning")
+    print ("3.Skæri, blað, steinn")
+    print ("4.Strengur")
+    print ("5.Heiltölur")
+    print ("6.Teningarspilið Craps")
+    print ("7.Teningakast")
+    print ("8.Byggingaupplýsingar úr hópverkefni 1")
+    print ("9.Hætta")
+    print (" ")
+
+        #liður 1 - Bílar
+    if svar = 1:
+
+        #liður 2 - Samlagning
+    if svar = 2:
+
+        #liður 3 - Skæri, blað, steinn
+    if svar = 3:
+        listi = ["blað", "skæri", "steinn"]
+        tölva = listi[randint(0,2)]
+        notandi = False
+        while notandi == False:
+            notandi = input("skæri, blað, steinn? [skrifaðu orðinn eins og stendur hér] ")
+        if notandi == tölva:
+            print ("jafntefli")
+
+        #liður 4 - Strengur
+    if svar = 4:
+
+        #liður 5 - Heiltölur
+    if svar = 5:
+
+        #liður 6 - Teningaspilið Craps
+    if svar = 6:
+
+        #liður 7 - Teningakast
+    if svar = 7:
+
+        #liður 8 - Byggingaupplýsingar úr hópverkefni 1
+    if svar = 8:
+
+    if svar = 9:
+        quit()
+
+    
